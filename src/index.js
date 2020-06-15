a26e77a55a3ae5ad4e22db70754164ecd8312f59
Maren Parnas Gulnes <maren.gulnes@bouvet.no>
2020-06-15 22:46:29 +0200

    allow mutations again

--- src/index.js
+++ src/index.js
@@ -20,10 +20,10 @@ const app = express();
 
 const schema = makeAugmentedSchema({
   typeDefs,
-  config: {
-    query: true, //default
-    mutation: false
-  }
+  // config: {
+  //   query: true, //default
+  //   mutation: false
+  // }
 });
 
 /*
